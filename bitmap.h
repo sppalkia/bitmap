f12f8755d9a3ffd15769e7b2cc39a493eb972bd0
Shoumik Palkar <shoumikpalkar@gmail.com>
2014-02-27 12:34:47 -0800

    Fixed function descriptions in bitmap.h

--- bitmap.h
+++ bitmap.h
@@ -9,15 +9,15 @@ typedef uint8_t* BitMap_t;
  * Allocate a bitmap with the input size
  *
  * Return: struct representing a new bitmap
- * Side Effects: None 
+ * Side Effects: errno set to ENOMEM if could not allocate 
  */
 BitMap_t alloc_bitmap(unsigned int);
 
 /*
  * Free the bitmap
  *
- * Return: None
- * Side Effects: input pointer is invalidated 
+ * Return: 0 on success, nonzero value on error
+ * Side Effects: errno set to EINVAL if invalid bitmap 
  */
 int check_bitmap(BitMap_t, unsigned int);
 
@@ -30,7 +30,7 @@ int check_bitmap(BitMap_t, unsigned int);
 void free_bitmap(BitMap_t);
 
 /*
- * Set the passed value to 1 in the bitmap
+ * Set the passed index to 1 in the bitmap
  *
  * Return: 0 if success, nonzero otherwise.
  * Side Effects: errno set
